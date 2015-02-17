13793f723f580b372eaa7e5215ecf97a5b9e5efc
Tim Smith <tim@cozy.co>
2015-02-17 12:17:17 -0800

    Remove duplicate require

--- lib/template.rb
+++ lib/template.rb
@@ -16,8 +16,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require 'json'
-
 module Singel
   # instance of a single packer template with methods to validate and extract data
   class PackerTemplate
